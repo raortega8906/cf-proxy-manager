6f1574db107b84990283c73bb1d8988d777e6d20
rortega8906 <raortega8906@gmail.com>
2026-03-14 00:09:06 +0100

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Rafael A. Ortega',
+            'email' => 'raortega8906@gmail.com',
+            'password' => bcrypt('Lala..1993'),
         ]);
     }
 }
