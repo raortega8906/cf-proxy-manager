91c68c753dcc8c61a04923b9394c7eafa9fcace5
rortega8906 <raortega8906@gmail.com>
2026-03-18 00:18:08 +0100

    add view index of logs

--- resources/views/logs/index.blade.php
+++ resources/views/logs/index.blade.php
@@ -7,5 +7,36 @@
 
 @section('content')
 
+<div class="card">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Sitio</th>
+                <th>Acción</th>
+                <th>Motivo</th>
+                <th>Estado</th>
+                <th>Mensaje</th>
+                <th>Cuándo</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse($logs as $log)
+            <tr>
+
+            </tr>
+            @empty
+            <tr>
+                <td colspan="6" style="text-align:center;color:var(--muted);padding:32px">
+                    Sin registros.
+                </td>
+            </tr>
+            @endforelse
+        </tbody>
+    </table>
+
+    {{-- <div class="pagination">
+        {{ $logs->links() }}
+    </div> --}}
+</div>
 
 @endsection
