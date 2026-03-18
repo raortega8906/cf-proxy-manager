992f51d5a4c04140764e0b17fd20cc683be82be4
rortega8906 <raortega8906@gmail.com>
2026-03-18 17:51:49 +0100

    add writeLogs function for future refactor

--- app/Services/CloudflareService.php
+++ app/Services/CloudflareService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\ProxyLog;
 use App\Models\ProxySite;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
@@ -14,6 +15,17 @@ class CloudflareService
 
     private string $api_url;
     private array $headers;
+
+    private function writeLogs(ProxySite $site, string $action, string $reason, string $status, string $message): void
+    {
+        ProxyLog::create([
+            'action' => $action,
+            'reason' => $reason,
+            'status' => $status,
+            'message' => $message, 
+            'site_id' => $site->id
+        ]);
+    }
 
     public function __construct()
     {
