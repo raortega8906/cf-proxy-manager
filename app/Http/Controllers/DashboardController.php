1e68737535efe4d63e1a1e322092a03b561e1b7a
rortega8906 <raortega8906@gmail.com>
2026-03-24 21:36:08 +0100

    refactor code in dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -29,8 +29,8 @@ class DashboardController extends Controller
         $countEnabled = $sites->where('proxy_enabled', true)->count();
         $countLaLiga = $sites->where('affected_by_laliga', true)->count();
         $countSsl = $sites->where('ssl_auto_renewal', true)->count();
-        $countSchedulePending =  $schedules->where('status', 'pending')->count();
-        $schedulePendingActive = ProxySchedule::whereIn('status', ['pending', 'active'])->paginate(5);
+        $countSchedulePending =  ProxySchedule::where('status', 'pending')->count();
+        $schedulePendingActive = ProxySchedule::whereIn('status', ['pending', 'active'])->latest()->paginate(5);
 
         return view('dashboard', compact('sites', 'logs', 'schedulePendingActive', 'schedules', 'countEnabled', 'countLaLiga', 'countSsl', 'countSchedulePending'));
     }
