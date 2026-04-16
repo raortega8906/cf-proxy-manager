65ab8180f5a14ed64543eb0257d8af1c9d4fe618
rortega8906 <raortega8906@gmail.com>
2026-04-16 23:11:21 +0200

    update route

--- routes/auth.php
+++ routes/auth.php
@@ -12,9 +12,11 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
+
+    // Ruta de registro comentado hasta la próximas verisones
     // Route::get('register', [RegisteredUserController::class, 'create'])
     //     ->name('register');
-
+    
     // Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('/register', function () {
