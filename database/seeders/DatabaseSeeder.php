4f8f599159b367499f9469a52f05caef8e635699
rortega8906 <raortega8906@gmail.com>
2026-04-16 23:05:48 +0200

    chore: DatabaseSeeder.php

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin CF P Manager',
+            'email' => 'manager@cfproxy.es',
+            'password' => Hash::make('otro2026'),
+        ]);
+    }
+}
